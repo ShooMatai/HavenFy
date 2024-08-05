461e678a90ce2acbf2af8bfae2679f9f4d33f3e6
Shoomatai <esemenov3356@gmail.com>
2024-08-05 16:48:39 +0300

    added pc optimization

--- home.js
+++ home.js
@@ -72,6 +72,39 @@ $(document).ready(function () {
         closeSearch();
       }
     });
+
+    $('main ul').first().hover(
+      function() {
+        $('main ul li').last().fadeToggle(450);
+      },
+      function() {
+        $('main ul li').last().fadeToggle(450);
+      });
+
+      const h1p = $('.h1Popular').text();
+      const h1n = $('.h1New').text();
+
+      $('main ul li').last().on('click', () => {
+        if($('.h1New').last().text() === h1n) {
+          $('main .new').fadeToggle(1);
+          $('main .new').css({
+            'display': 'flex'
+          })
+          $('.new-films, .new-shows').css({
+            'flex-direction': 'row',
+            'gap': '8%',
+            'width': '100%'
+          })
+          $('main .popular').fadeToggle(1);
+          $('.h1Popular').text(h1n);
+          $('.h1New').text(h1p);
+        } else {
+          $('main .new').fadeToggle(1);
+          $('main .popular').fadeToggle(1);
+          $('.h1Popular').text(h1p);
+          $('.h1New').text(h1n);
+        }
+      })
   } else {
     $(".closed").removeClass("closed").addClass("opened");
 
@@ -81,5 +114,38 @@ $(document).ready(function () {
     $(".opened").blur(() => {
       $(".searchImg").attr("src", "img/search.svg");
     });
+
+    $('main ul').first().hover(
+      function() {
+        $('main ul li').last().fadeToggle(450);
+      },
+      function() {
+        $('main ul li').last().fadeToggle(450);
+      });
+
+      const h1p = $('.h1Popular').text();
+      const h1n = $('.h1New').text();
+
+      $('main ul li').last().on('click', () => {
+        if($('.h1New').last().text() === h1n) {
+          $('main .new').fadeToggle(1);
+          $('main .new').css({
+            'display': 'flex'
+          })
+          $('.new-films, .new-shows').css({
+            'flex-direction': 'row',
+            'gap': '8%',
+            'width': '100%'
+          })
+          $('main .popular').fadeToggle(1);
+          $('.h1Popular').text(h1n);
+          $('.h1New').text(h1p);
+        } else {
+          $('main .new').fadeToggle(1);
+          $('main .popular').fadeToggle(1);
+          $('.h1Popular').text(h1p);
+          $('.h1New').text(h1n);
+        }
+      })
   }
 });
